e44636c31ea01968f011859cd54caec5e85e481c
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-08-30 10:43:14 +0300

    Add Lottie animation

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # SampleLottie
-Example of using Airbnb Lottie animations in Android app
+Example of using Airbnb Lottie animations in Android app.
+
+## Sources
+### Airbnb Lottie
+https://github.com/airbnb/lottie-android/
+
+http://airbnb.io/lottie/
+
+### Lottie Animation Collection
+https://www.lottiefiles.com/
+
+Animation used in the app:
+
+https://www.lottiefiles.com/72-favourite-app-icon
--- app/src/main/java/com/gpetuhov/android/samplelottie/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/samplelottie/MainActivity.kt
@@ -2,11 +2,14 @@ package com.gpetuhov.android.samplelottie
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        animation_view.setOnClickListener { if (!animation_view.isAnimating) animation_view.playAnimation() }
     }
 }
